2b6830f58eff72439acd76105ca40f5587d54f7c
Olexii Antonenko <antonenkoolexiy@gmail.com>
2023-12-02 17:41:46 +0200

    heroku1

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express'
 const app = express()
 
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello Yulia')
 })
 
 app.get('/status', function (req, res) {
     res.send('Status')
   })
 
-app.listen(PORT, () => console.log(`Server opened on port ${PORT}`))
+app.listen(port, () => console.log(`Server opened on port ${port}`))
